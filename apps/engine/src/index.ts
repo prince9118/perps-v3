a6d68da6be6d8468361e6b7c1fbe23b075765739
prince9118 <pchaudharyskn@gmail.com>
2026-07-23 16:53:06 +0530

    engine

--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -1,6 +1,7 @@
 import { redis } from "@repo/redis";
 
 async function main() {
+  //   console.log("Engine Started");
   while (true) {
     const result = await redis.xread(
       "BLOCK",
@@ -9,6 +10,7 @@ async function main() {
       "order_events",
       "$"
     );
+    if (!result) continue;
     console.log(result);
   }
 }
