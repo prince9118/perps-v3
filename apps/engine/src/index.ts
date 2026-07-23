1de975a294ebe875509d8e34e2b53a9ad100f6f2
prince9118 <pchaudharyskn@gmail.com>
2026-07-23 22:53:21 +0530

    redis

--- apps/engine/src/index.ts
+++ apps/engine/src/index.ts
@@ -1,7 +1,18 @@
 import { redis } from "@repo/redis";
+function parseRedisFields(fields: string[]) {
+  const obj: Record<string, string> = {};
+
+  for (let i = 0; i < fields.length; i += 2) {
+    const key = fields[i]!;
+    const value = fields[i + 1]!;
+
+    obj[key] = value;
+  }
+  return obj;
+}
 
 async function main() {
-  //   console.log("Engine Started");
+  console.log("Engine Started");
   while (true) {
     const result = await redis.xread(
       "BLOCK",
@@ -11,7 +22,15 @@ async function main() {
       "$"
     );
     if (!result) continue;
-    console.log(result);
+    const [streamName, messages] = result[0];
+    for (const [messageId, fields] of messages) {
+      const parsedFields = parseRedisFields(fields);
+      const event = {
+        type: parsedFields.type,
+        data: JSON.parse(parsedFields.data)
+      };
+      lastId=messageId;
+    }
   }
 }
 main();
