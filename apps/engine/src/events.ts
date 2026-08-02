3af13772e4966b34116eca6acfc254f98a7e757f
prince9118 <pchaudharyskn@gmail.com>
2026-08-03 03:15:51 +0530

    positions

--- apps/engine/src/events.ts
+++ apps/engine/src/events.ts
@@ -1,15 +1,1 @@
-import { type EngineOrder, type EngineFill } from "@repo/types";
 
-export type EngineEvent =
-  | {
-      type: "ORDER_CREATED";
-      data: EngineOrder;
-    }
-  | {
-      type: "ORDER_UPDATED";
-      data: EngineOrder;
-    }
-  | {
-      type: "TRADE_CREATED";
-      data: EngineFill;
-    };
