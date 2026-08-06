5c735204b379b3c45f36fcf1cbc8c4493c04dfcd
prince9118 <pchaudharyskn@gmail.com>
2026-08-07 01:03:20 +0530

    orders

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
-import orderRoutes from "./routes/orders.routes";
+import orderRoutes from "./routes/order.routes";
 
 const app = express();
 
